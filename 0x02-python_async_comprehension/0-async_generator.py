d572dcc101676169822243de1f85283ba5220917
AnasJafir <dr.anas.jafir@gmail.com>
2024-06-11 15:51:25 -0700

    Creating a coroutine that yields a random number between 0 and 10

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -20,5 +20,5 @@ async def async_generator() -> Generator[float, None, None]:
 
     """
     for _ in range(10):
-        time.sleep(1)  # wait for 1 second
-        yield random.random() * 10  # yield a random number between 0 and 10
+        time.sleep(1)
+        yield random.random() * 10
