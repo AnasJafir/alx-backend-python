26de80b5d9d1f2ae4c4eb1aa6b2fff4ea80fd5b2
AnasJafir <dr.anas.jafir@gmail.com>
2024-06-11 15:50:36 -0700

    Creating a coroutine that yields a random number between 0 and 10

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Asynchronous generator that yields random floating point numbers.
+
+This generator will produce 10 random numbers between 0 and 10
+with a 1-second delay between each number.
+"""
+import asyncio
+import random
+import time
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Asynchronous generator that yields random floating point numbers.
+
+    Yields:
+        float: A random floating point number between 0 and 10.
+
+    """
+    for _ in range(10):
+        time.sleep(1)  # wait for 1 second
+        yield random.random() * 10  # yield a random number between 0 and 10
