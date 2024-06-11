f831778947aba8909b14af04d50893a37bf78dca
AnasJafir <dr.anas.jafir@gmail.com>
2024-06-11 08:30:16 -0700

    Creating a function that returns the average of execution time of a given number of coroutines

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the execution time of wait_n function
+    and returns the average time per coroutine execution.
+
+    Parameters:
+    - n: int
+        The number of times to repeat the wait_n function.
+    - max_delay: int
+        The maximum delay in seconds.
+
+    Returns:
+    - float
+        The average time per coroutine execution in seconds.
+    """
+    # Record the start time
+    start_time = time.time()
+
+    # Call wait_n function num_iterations times
+    await wait_n(n, max_delay)
+
+    # Record the end time
+    end_time = time.time()
+
+    # Calculate the average time per iteration
+    total_time = (end_time - start_time) / n
+
+    # Return the average time per iteration
+    return total_time
